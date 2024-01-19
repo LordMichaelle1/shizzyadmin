10ea26e7c2407e8ef703f583cc661b146b5c7070
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-01-19 01:30:21 +0100

    updt

--- src/admindash.jsx
+++ src/admindash.jsx
@@ -1,4 +1,6 @@
-import "./dashboard.css"
+import "./dashboard.css";
+import Admin from "./assets/user.png"
+import Dummy from "./assets/third.webp"
 
 function Dashboard() {
   return (
@@ -6,13 +8,113 @@ function Dashboard() {
       <header>
         <h1>Welcome Admin</h1>
         <section>
-          {/* <img src="" alt="" /> */}
+          <img src={Admin} />
           <p>John Doe</p>
         </section>
       </header>
-      <div>
-        <aside></aside>
-        <main></main>
+
+      <div className="admin-box">
+        <aside className="left-nav">
+          <div className="left-nav-top">
+            <img src={Admin} />
+            <section>
+              <h1>John Doe</h1>
+              <p>Admnistrator</p>
+            </section>
+          </div>
+          <ul>
+            <li id="special">Dashboard</li>
+            <li>Create Article</li>
+            <li>View Admin Details</li>
+          </ul>
+        </aside>
+        <main className="admin-main">
+          <h1>Dashboard</h1>
+
+          <section>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+              <article>
+              <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio magnam mollitia odit assumenda veniam vitae praesentium enim id, rem nulla ipsum quia nemo error? Earum corporis quod vitae! Modi, harum.</p>
+              <button type="submit">Delete</button>
+              </article>
+            </div>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+                <article>
+                  <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis quis a numquam magni, sequi totam voluptatibus, consectetur sunt rem nisi iusto quisquam, odit voluptatem dolores culpa sit amet omnis.</p>
+                  <button type="submit">Delete</button>
+                </article>
+            </div>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+                <article>
+                  <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis quis a numquam magni, sequi totam voluptatibus, consectetur sunt rem nisi iusto quisquam, odit voluptatem dolores culpa sit amet omnis.</p>
+                  <button type="submit">Delete</button>
+                </article>
+            </div>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+                <article>
+                  <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis quis a numquam magni, sequi totam voluptatibus, consectetur sunt rem nisi iusto quisquam, odit voluptatem dolores culpa sit amet omnis.</p>
+                  <button type="submit">Delete</button>
+                </article>
+            </div>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+                <article>
+                  <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis quis a numquam magni, sequi totam voluptatibus, consectetur sunt rem nisi iusto quisquam, odit voluptatem dolores culpa sit amet omnis.</p>
+                  <button type="submit">Delete</button>
+                </article>
+            </div>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+                <article>
+                  <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis quis a numquam magni, sequi totam voluptatibus, consectetur sunt rem nisi iusto quisquam, odit voluptatem dolores culpa sit amet omnis.</p>
+                  <button type="submit">Delete</button>
+                </article>
+            </div>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+                <article>
+                  <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis quis a numquam magni, sequi totam voluptatibus, consectetur sunt rem nisi iusto quisquam, odit voluptatem dolores culpa sit amet omnis.</p>
+                  <button type="submit">Delete</button>
+                </article>
+            </div>
+            <div>
+              <aside>
+                <img src={Dummy} />
+              </aside>
+                <article>
+                  <h2>Athene Network Review – How to mine ATH Coin, Legit or Scam?</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis quis a numquam magni, sequi totam voluptatibus, consectetur sunt rem nisi iusto quisquam, odit voluptatem dolores culpa sit amet omnis.</p>
+                  <button type="submit">Delete</button>
+                </article>
+            </div>
+          </section>
+          
+        </main>
       </div>
     </div>
   );
