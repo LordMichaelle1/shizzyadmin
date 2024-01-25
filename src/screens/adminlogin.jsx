1677ec869cbea1eb4c95575ff089bc5f18ad8dbd
LordMichaelle1 <97275091+LordMichaelle1@users.noreply.github.com>
2024-01-25 15:46:28 +0100

    Update adminlogin.jsx

--- src/screens/adminlogin.jsx
+++ src/screens/adminlogin.jsx
@@ -6,12 +6,12 @@ function AdminLogin() {
         <div className="admin-login">
             <div id="login">
                 <h1>Admin Login</h1>
-                <form action="">
-                    <input required type="email" placeholder="Email/Username" />
-                    <input required id="lock" type="password" placeholder="Password"/>
-                    <button type="submit"><NavLink class to="/dashboard">  Log In </NavLink></button> 
+                <form action="http://localhost:80/shizzyadmin-master/php/loginvalid.php" method="POST">
+                    <input required type="email" placeholder="Email/Username" name="email" />
+                    <input required id="lock" type="password" placeholder="Password" name="pass"/>
+                    <input type="submit" value="Log in" name="subBtn" />
+                    {/* <button type="submit"><NavLink class to="/dashboard">  Log In </NavLink></button>  */}
                 </form>
-
                 <NavLink className="forgot-password" to="/forgotpassword"> forgot username/password </NavLink>
             </div>
         </div>
